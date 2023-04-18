ba60fb4106647f63b34044f56e3ab3e821f045af
guille <guillermorojo42@gmail.com>
2023-04-18 18:39:49 +0200

    a

--- src/arbolNario/Pruebas.java
+++ src/arbolNario/Pruebas.java
@@ -2,7 +2,7 @@ package arbolNario;
 
 public class Pruebas {
 	
-	// Pruebas ------------------------------------------------------------
+	// Pruebas ------------------------------------------------------------hola
 	public static void main(String[] args) {
 		Pruebas pruebas = new Pruebas();
 		pruebas.ejecutar();
